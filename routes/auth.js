9cae5fbe61174f017892fb9b895fcd382f62095d
poojanandita87 <poojanandita87gmail.com>
2026-07-11 21:50:27 +0530

    Initial commit (clean)

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const user = require("../models/user");
 
 // Signup
 router.post("/signup", async (req, res) => {

60b22ef0ba9a466d7bd059c084add4dcdc401af6
poojanandita87 <poojanandita87gmail.com>
2026-07-16 19:47:12 +0530

    Initial commit (clean)

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors({
 app.use(express.json());
 
 // Serve frontend
-app.use(express.static(path.join(__dirname,".." )));
+app.use(express.static(path.join(__dirname )));
 
 // API routes
 app.use("/api/auth", authRoutes);
@@ -30,7 +30,7 @@ app.use("/api/history", historyRoutes);
 
 // Default route
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"..", "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 5000;
